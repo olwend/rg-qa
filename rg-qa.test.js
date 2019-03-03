8762cd24c90413d42acae7dee19f4601d15d31fc
olwend <olwend@gmail.com>
2019-03-03 19:42:57 +0000

    working test ensures QA position still advertised

--- rg-qa.test.js
+++ rg-qa.test.js
@@ -1,3 +1,5 @@
+jest.setTimeout(15000);
+
 describe('QA position is in rewardgateway', () => {
   beforeAll(async () => {
     await page.goto('https://www.rewardgateway.com/careers');
@@ -7,6 +9,21 @@ describe('QA position is in rewardgateway', () => {
     await expect(page.title()).resolves.toMatch('Careers | Reward Gateway');
     await page.screenshot({path: './careers.png'});
   });
-});
 
-// https://www.rewardgateway.com/careers
+  it('should contain a role called "Automation QA"', async () => {
+    await page.waitForSelector('#jobs-container');
+    let result = await page.evaluate(() => document.body.textContent);
+      expect(result).toContain('Automation QA')
+  });
+
+  it('should contain "Jest or Mocha experience" specific to the role', async () => {
+    await page.click('#jobs-container > div > div:nth-child(1) > a');
+    await page.waitFor(2000);
+    await page.screenshot({path: './automationQA.png'});
+    const selector = 'body > div.content-wrapper.posting-page > div';
+    const text = 'Jest or Mocha experience';
+    const selected = await page.$eval(selector, el => el.textContent);
+        expect(selected).toContain(text);
+    // console.log(selected);
+  });
+});
